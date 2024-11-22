e6ab390846ead152cb724218781b5bd3593115f2
sakul-the-one <68478969+sakul-the-one@users.noreply.github.com>
2024-11-22 14:21:58 +0100

    added everything

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <ti/getkey.h>
+#include <ti/vars.h>
+#include "3D/3DGraphics.h"
+
+int main() 
+{
+    D3G_Init();
+    real_t x,y,z;
+    os_GetMatrixElement(OS_VAR_MAT_I, 1,1, &x);
+    os_GetMatrixElement(OS_VAR_MAT_I, 1,2, &y);
+    os_GetMatrixElement(OS_VAR_MAT_I, 1,3, &z);
+    Vector3 Point1 = {os_RealToFloat(&x),os_RealToFloat(&y),os_RealToFloat(&z)};
+    Vector3 Point2 = {2,3,10};
+    Vector3 Point3 = {5,10,1};
+    D3G_DrawPoint(Point1);
+    D3G_DrawLine(Point2, Point3);
+    os_GetKey();
+    D3G_Destroy();
+    return 0;
+}
